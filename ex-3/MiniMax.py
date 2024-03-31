65210fca0a38c52f4ed27267fac0b10effca9538
Kuba Marchut <kuba.marchut@gmail.com>
2024-03-31 23:24:44 +0200

    probably working minmax demo

--- ex-3/MiniMax.py
+++ ex-3/MiniMax.py
@@ -1,31 +1,39 @@
 import numpy as np
-from typing import Tuple
+from typing import Tuple, List
 from math import inf
 
-from TicTacToeHeuristic import TicTacToe_heuristic
+from TicTacToeHeuristic import evaluate_game, is_terminal
 
 
-def evaluate_game(state: np.ndarray) -> int:
-    raise NotImplementedError
+def possible_moves(state: np.ndarray, player: bool) -> List[np.ndarray]:
+    player_marker = 1 if player else -1
+    empty_positions = list(zip(*np.where(state == 0)))
+    return [apply_move(state, pos, player_marker) for pos in empty_positions]
+
+
+def apply_move(
+    state: np.ndarray, position: Tuple[int, int], player_marker: int
+) -> np.ndarray:
+    next_state = state.copy()
+    next_state[position] = player_marker
+    return next_state
 
 
 def minmax(
-    state: np.ndarray,
-    depth: int,
-    maximizing: bool,
-    action: Tuple[int, int],
+    state: np.ndarray, action: Tuple[int, int], maximizing: bool, depth: int
 ) -> int:
-    if depth == 0:
-        return TicTacToe_heuristic(3, state)
+    if is_terminal(state) != None:
+        return evaluate_game(state)
+
     if maximizing:
         value = -inf
-        for child in state:
-            value = max(value, minmax(child, depth - 1, False, (0, 0)))
+        for child in possible_moves(np.copy(state), maximizing):
+            value = max(value, minmax(child, (0, 0), False, depth - 1))
         return value
     else:
         value = inf
-        for child in state:
-            value = min(value, minmax(child, depth - 1, True, (0, 0)))
+        for child in possible_moves(np.copy(state), maximizing):
+            value = min(value, minmax(child, (0, 0), True, depth - 1))
         return value
 
 
@@ -40,4 +48,36 @@ def alpha_pruning(
 
 
 if __name__ == "__main__":
-    print(minmax([(1, 1)], 1, False, (0, 0)))
+    starting_board = np.zeros(shape=(3, 3))
+
+    starting_board = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+
+    # print(minmax(starting_board, (1, 1), False, 10))
+    # print(minmax(starting_board, (1, 1), True, 10))
+
+    board = starting_board
+    for i in range(9):
+        if i % 2 == 0:
+            best = -inf
+        else:
+            best = inf
+        chosen = np.zeros(shape=(3, 3))
+        for move in possible_moves(board, i % 2 == 0):
+            if i % 2 == 0:
+                move_score = minmax(move, (1, 1), False, 10)
+                # print(move, move_score)
+                if best < move_score:
+                    best = move_score
+                    chosen = move
+
+            else:
+                move_score = minmax(move, (1, 1), True, 10)
+                # print(move, move_score)
+                if best > move_score:
+                    best = move_score
+                    chosen = move
+
+        print(10 * "-")
+        print(chosen, best)
+        print(10 * "-")
+        board = chosen
--- ex-3/TicTacToeHeuristic.py
+++ ex-3/TicTacToeHeuristic.py
@@ -1,8 +1,8 @@
 import numpy as np
-from typing import Tuple
+from typing import Optional, Tuple
 
 
-def TicTacToe_heuristic(size: int, move: Tuple[int, int]):
+def heuristic_matrix(size: int = 3) -> np.ndarray:
     matrix = np.full((size, size), size)
     center = size // 2
     matrix[center][center] += 1
@@ -11,18 +11,56 @@ def TicTacToe_heuristic(size: int, move: Tuple[int, int]):
     matrix[0][center] -= 1
     matrix[size - 1][center] -= 1
 
-    return matrix[move[0]][move[1]]
+    return matrix
+
+
+def is_terminal(state: np.ndarray) -> Optional[int]:
+    if check_for_winner(state) != None:
+        return check_for_winner(state)
+    elif np.count_nonzero(state) == state.size:
+        return 0
+    else:
+        return None
+
+
+def evaluate_game(state: np.ndarray) -> int:
+    if is_terminal(state) != None:
+        return is_terminal(state) * 10
+    else:
+        return np.sum(np.multiply(heuristic_matrix(), state))
+
+
+def check_for_winner(state: np.ndarray) -> Optional[int]:
+    for player in [-1, 1]:
+        if np.any(np.all(state == player, axis=0)) or np.any(
+            np.all(state == player, axis=1)
+        ):
+            return player
+
+        if np.all(np.diag(state) == player) or np.all(
+            np.diag(np.fliplr(state)) == player
+        ):
+            return player
+
+    return None
 
 
 if __name__ == "__main__":
-    print(TicTacToe_heuristic(3, (0, 0)))
-    print(TicTacToe_heuristic(3, (0, 1)))
-    print(TicTacToe_heuristic(3, (0, 2)))
+    """print(heuristic_matrix((0, 0)))
+    print(heuristic_matrix((0, 1)))
+    print(heuristic_matrix((0, 2)))
 
-    print(TicTacToe_heuristic(3, (1, 0)))
-    print(TicTacToe_heuristic(3, (1, 1)))
-    print(TicTacToe_heuristic(3, (1, 2)))
+    print(heuristic_matrix((1, 0)))
+    print(heuristic_matrix((1, 1)))
+    print(heuristic_matrix((1, 2)))
 
-    print(TicTacToe_heuristic(3, (2, 0)))
-    print(TicTacToe_heuristic(3, (2, 1)))
-    print(TicTacToe_heuristic(3, (2, 2)))
+    print(heuristic_matrix((2, 0)))
+    print(heuristic_matrix((2, 1)))
+    print(heuristic_matrix((2, 2)))
+    """
+    test_board = np.array([[1, 1, 1], [0, 0, 0], [0, 0, 0]])
+    test_board = np.array([[0, 0, 0], [1, 0, 1], [0, 0, 0]])
+    test_board = np.array([[0, 0, 0], [0, 0, 0], [1, 1, 1]])
+
+    print(is_terminal(test_board))
+    print(evaluate_game(test_board))
