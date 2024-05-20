5ab6b530f7199485a18ff8ce499c1674e80bd583
Kuba Marchut <kuba.marchut@gmail.com>
2024-05-20 17:46:03 +0200

    fixing matrix mul errors

--- ex-5/main.py
+++ ex-5/main.py
@@ -1,13 +1,23 @@
 from train import *
 from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 
 def main():
-    layers = [10, 5]
     digits = load_digits()
+    input, output = digits.data, digits.target
 
-    train_model([], [], layers)
+    output = np.eye(10)[output]
+
+    input_train, input_test, output_train, output_test = train_test_split(
+        input, output, test_size=0.3, random_state=10
+    )
+
+    num_class = 10
+
+    layers = [input.shape[1], 15, num_class]
+    train_model(input_train, output_train, layers)
 
 
 if __name__ == "__main__":
--- ex-5/train.py
+++ ex-5/train.py
@@ -1,12 +1,34 @@
 import numpy as np
+from activation import relu, sigmoid
 
 
 def mse(y, y_received):
     return np.mean((y - y_received) ** 2)
 
 
-def forward():
-    pass
+def forward(data, network):
+    new_net = {}
+    input = data
+
+    layers_cnt = (len(network) // 2) - 1
+
+    for layer_ind in range(layers_cnt):
+        output = (
+            np.dot(input, network[f"weight({layer_ind})"])
+            + network[f"bias({layer_ind})"]
+        )
+        input = relu(output)
+        new_net[f"output({layer_ind})"] = output
+        new_net[f"input({layer_ind})"] = input
+
+    output = (
+        np.dot(input, network[f"weight({layers_cnt})"]) + network[f"bias({layers_cnt})"]
+    )
+    input = sigmoid(output)
+    new_net[f"output({layers_cnt})"] = output
+    new_net[f"input({layers_cnt})"] = input
+
+    return input, new_net
 
 
 def backward():
@@ -17,24 +39,27 @@ def init_net(layers):
     np.random.seed(123)
     network = {}
 
-    for layer_ind, layer in enumerate(layers):
-        network[f"weight({layer_ind})"] = np.random.randn(layer)
+    for layer_ind, layer in enumerate(layers[1:]):
+        network[f"weight({layer_ind})"] = np.random.randn(layers[layer_ind], layer)
         network[f"bias({layer_ind})"] = np.zeros((1, layer))
 
     return network
 
 
 def train_model(
-    input_train, output_train, layers, learing_rate=0.01, epochs=10, batch_size=32
+    data_train, result_train, layers, learing_rate=0.01, epochs=10, batch_size=32
 ):
     network = init_net(layers)
-    print(network)
     not_freq = epochs // 10
 
     for epoch in range(epochs):
+        for i in range(0, data_train.shape[0], batch_size):
+            data_batch = data_train[i : i + batch_size]
+            result_batch = result_train[i : i + batch_size]
+
+            res, cache_net = forward(data_batch, network)
 
         if epoch % not_freq == 0:
-            output_current = forward()
-            # training_loss = mse(output_train, output_current)
-            training_loss = 0
+            result_current, cache_net = forward(data_train, network)
+            training_loss = mse(result_train, result_current)
             print(f"Epoch: {epoch+1}, Loss: {training_loss}")
