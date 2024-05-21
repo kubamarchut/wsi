122d151c3458801402a13ae05aba188eaf32f7a7
Mateusz Szyperek <01169362@pw.edu.pl>
2024-05-21 02:47:24 +0200

    Testing, a lot of it

--- ex-5/main.py
+++ ex-5/main.py
@@ -3,9 +3,42 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+import multiprocessing
 
+
+def brain_process(input, layers, input_train, output_train, input_test, output_test, proc, epochs, batch, return_dict, freq,
+                  print_plots=True):
+    layers_bis = [input.shape[1], layers, 10]  # 10 for num_class
+    brain = Brain_network(input_train, output_train, layers_bis)
+    model = Brain_network.train_model(brain, input_train, output_train, layers_bis, epochs=epochs, batch_size=batch,
+                                      freq=freq)
+    eval = Brain_network.evaluate_model(brain, input_test, output_test, model)
+    if print_plots:
+        plt.plot(brain.probed_epoch, brain.losses)
+        plt.title(f"Training Loss, epochs: {epochs}, batch size:{batch}, layers:{layers}")
+        plt.xlabel("epochs")
+        plt.ylabel("loss")
+        plt.grid()
+        plt.show()
+    key_full = f"E:{epochs}, B:{batch}, L:{layers}"
+    return_dict[proc] = [key_full, eval, max(brain.losses), min(brain.losses), epochs, batch, np.std(brain.losses), layers]
+    # print(key_full, "-", eval)
+
+
+def quick_mean(list):
+    calc_ls=[]
+    for l in list:
+        calc_ls.append(l[1])
+    sum_ = 0
+    for i in calc_ls:
+        sum_ += i
+    return sum_/len(calc_ls)
 
 def main():
+
+    print_plots = True
+    layers = 15
+
     digits = load_digits()
     input, output = digits.data, digits.target
 
@@ -15,15 +48,153 @@ def main():
         input, output, test_size=0.3, random_state=10
     )
 
-    num_class = 10
-
     scaler = StandardScaler()
     input_train = scaler.fit_transform(input_train)
     input_test = scaler.transform(input_test)
 
-    layers = [input.shape[1], 15, num_class]
-    model = train_model(input_train, output_train, layers)
-    evaluate_model(input_test, output_test, model)
+    results = []
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    jobs = []
+    proc = 0
+    freq = 1000
+
+    for epochs in [100, 200, 500, 1000, 1500, 2000, 2500]:
+        for batch in [32, 64, 96, 128]:
+
+            proc += 1
+            p = multiprocessing.Process(target=brain_process,
+                                            args=(input, layers, input_train, output_train, input_test, output_test, proc, epochs,
+                                                  batch, return_dict, freq, print_plots))
+            jobs.append(p)
+            p.start()
+    for p in jobs:
+        p.join()
+
+    for e in return_dict.values():
+        results.append(e)
+
+    eval_b32 = []
+    for t in results:
+        if "B:32" in str(t[0]):
+            eval_b32.append(t)
+    eval_b32_s = sorted(eval_b32, key=lambda x: x[1])
+    print("Best batch 32:", eval_b32_s[-1])
+
+    eval_b64 = []
+    for t in results:
+        if "B:64" in str(t[0]):
+            eval_b64.append(t)
+    eval_b64_s = sorted(eval_b64, key=lambda x: x[1])
+    print("Best batch 64:", eval_b64_s[-1])
+
+    eval_b96 = []
+    for t in results:
+        if "B:96" in str(t[0]):
+            eval_b96.append(t)
+    eval_b96_s = sorted(eval_b96, key=lambda x: x[1])
+    print("Best batch 96:", eval_b96_s[-1])
+
+    eval_b128 = []
+    for t in results:
+        if "B:128" in str(t[0]):
+            eval_b128.append(t)
+    eval_b128_s = sorted(eval_b128, key=lambda x: x[1])
+    print("Best batch 128:", eval_b128_s[-1])
+
+    eval_e100 = []
+    for t in results:
+        if "E:100" in str(t[0]):
+            eval_e100.append(t)
+    eval_e100_s = sorted(eval_e100, key=lambda x: x[1])
+    print("Best epoch 100:", eval_e100_s[-1])
+
+    eval_e200 = []
+    for t in results:
+        if "E:200" in str(t[0]):
+            eval_e200.append(t)
+    eval_e200_s = sorted(eval_e200, key=lambda x: x[1])
+    print("Best epoch 200:", eval_e200_s[-1])
+
+    eval_e500 = []
+    for t in results:
+        if "E:500" in str(t[0]):
+            eval_e500.append(t)
+    eval_e500_s = sorted(eval_e500, key=lambda x: x[1])
+    print("Best epoch 500:", eval_e500_s[-1])
+
+    eval_e1000 = []
+    for t in results:
+        if "E:1000" in str(t[0]):
+            eval_e1000.append(t)
+    eval_e1000_s = sorted(eval_e1000, key=lambda x: x[1])
+    print("Best epoch 1000:", eval_e1000_s[-1])
+
+    eval_e1500 = []
+    for t in results:
+        if "E:1500" in str(t[0]):
+            eval_e1500.append(t)
+    eval_e1500_s = sorted(eval_e1500, key=lambda x: x[1])
+    print("Best epoch 1500:", eval_e1500_s[-1])
+
+    eval_e2000 = []
+    for t in results:
+        if "E:2000" in str(t[0]):
+            eval_e2000.append(t)
+    eval_e2000_s = sorted(eval_e2000, key=lambda x: x[1])
+    print("Best epoch 2000:", eval_e2000_s[-1])
+
+    eval_e2500 = []
+    for t in results:
+        if "E:2500" in str(t[0]):
+            eval_e2500.append(t)
+    eval_e2500_s = sorted(eval_e2500, key=lambda x: x[1])
+    print("Best epoch 2500:", eval_e2500_s[-1])
+
+    print("Mean batch 32:", quick_mean(eval_b32_s))
+    print("Mean batch 64:", quick_mean(eval_b64_s))
+    print("Mean batch 96:", quick_mean(eval_b96_s))
+    print("Mean batch 128:", quick_mean(eval_b128_s))
+    print("Mean epoch 100:", quick_mean(eval_e100_s))
+    print("Mean epoch 200:", quick_mean(eval_e200_s))
+    print("Mean epoch 500:", quick_mean(eval_e500_s))
+    print("Mean epoch 1000:", quick_mean(eval_e1000_s))
+    print("Mean epoch 1500:", quick_mean(eval_e1500_s))
+    print("Mean epoch 2000:", quick_mean(eval_e2000_s))
+    print("Mean epoch 2500:", quick_mean(eval_e2500_s))
+
+
+
+    results_sorted = sorted(results, key=lambda x: (x[4], x[5], x[7]))
+    plt.figure(figsize=(16, 9))
+    for r in results_sorted:
+        plt.errorbar(r[0], r[1], fmt='o', capthick=2, capsize=5)
+    plt.title(f"Training Evaluations with F1 scores")
+    plt.xlabel("Epochs,Batch size")
+    plt.ylabel("Final evaluation")
+    plt.xticks(rotation=-60)
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(16, 9))
+    for r in results_sorted:
+        plt.errorbar(r[0], r[2], r[6], fmt='o', capthick=2, capsize=5)
+    plt.title(f"Training Max Loss")
+    plt.xlabel("Epochs,Batch size")
+    plt.ylabel("Loss")
+    plt.xticks(rotation=-60)
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(16, 9))
+    for r in results_sorted:
+        plt.errorbar(r[0], r[3], r[6], fmt='o', capthick=2, capsize=5)
+    plt.title(f"Training Min Loss")
+    plt.xlabel("Epochs,Batch size")
+    plt.ylabel("Loss")
+    plt.xticks(rotation=-60)
+    plt.grid()
+    plt.show()
 
 
 if __name__ == "__main__":
--- ex-5/train.py
+++ ex-5/train.py
@@ -7,112 +7,135 @@ def mse(y, y_received):
     return np.mean((y - y_received) ** 2)
 
 
-def forward(data, network):
-    new_net = {}
-    input = data
+class Brain_network(object):
+    def __init__(self, inp, outp, layr) -> None:
+        self.inp = inp
+        self.outp = outp
+        self.layr = layr
+        self.probed_epoch = []
+        self.losses = []
+        self.prints = {}
 
-    layers_cnt = (len(network) // 2) - 1
 
-    for layer_ind in range(layers_cnt):
+    def forward(self, data, network):
+        new_net = {}
+        input = data
+
+        layers_cnt = (len(network) // 2) - 1
+
+        for layer_ind in range(layers_cnt):
+            output = (
+                    np.dot(input, network[f"weight({layer_ind})"])
+                    + network[f"bias({layer_ind})"]
+            )
+            input = relu(output)
+            new_net[f"output({layer_ind})"] = output
+            new_net[f"input({layer_ind})"] = input
+
         output = (
-            np.dot(input, network[f"weight({layer_ind})"])
-            + network[f"bias({layer_ind})"]
+                np.dot(input, network[f"weight({layers_cnt})"]) + network[f"bias({layers_cnt})"]
         )
-        input = relu(output)
-        new_net[f"output({layer_ind})"] = output
-        new_net[f"input({layer_ind})"] = input
+        input = sigmoid(output)
+        new_net[f"output({layers_cnt})"] = output
+        new_net[f"input({layers_cnt})"] = input
 
-    output = (
-        np.dot(input, network[f"weight({layers_cnt})"]) + network[f"bias({layers_cnt})"]
-    )
-    input = sigmoid(output)
-    new_net[f"output({layers_cnt})"] = output
-    new_net[f"input({layers_cnt})"] = input
+        return input, new_net
 
-    return input, new_net
+    def backward(self, data, result, network, cache_net):
+        gradients = {}
+        layers_cnt = len(network) // 2
+        m = data.shape[0]
 
-
-def backward(data, result, network, cache_net):
-    gradients = {}
-    layers_cnt = len(network) // 2
-    m = data.shape[0]
-
-    # Ostatnia warstwa
-    out_last = cache_net[f"input({layers_cnt-1})"]
-    dZ = out_last - result
-    gradients[f"dweight({layers_cnt-1})"] = (
-        np.dot(cache_net[f"input({layers_cnt-1})"].T, dZ) / m
-    )
-    gradients[f"dbias({layers_cnt-1})"] = np.sum(dZ, axis=0, keepdims=True) / m
-
-    # Warstwy ukryte
-    for l in reversed(range(1, layers_cnt)):
-        dA = np.dot(dZ, network[f"weight({l})"].T)
-        dZ = dA * relu_dev(cache_net[f"output({l-1})"])
-        gradients[f"dweight({l-1})"] = (
-            np.dot(data.T if l == 1 else cache_net[f"input({l-2})"].T, dZ) / m
+        # Ostatnia warstwa
+        out_last = cache_net[f"input({layers_cnt - 1})"]
+        dZ = out_last - result
+        gradients[f"dweight({layers_cnt - 1})"] = (
+                np.dot(cache_net[f"input({layers_cnt - 1})"].T, dZ) / m
         )
-        gradients[f"dbias({l-1})"] = np.sum(dZ, axis=0, keepdims=True) / m
+        gradients[f"dbias({layers_cnt - 1})"] = np.sum(dZ, axis=0, keepdims=True) / m
 
-    return gradients
+        # Warstwy ukryte
+        for l in reversed(range(1, layers_cnt)):
+            dA = np.dot(dZ, network[f"weight({l})"].T)
+            dZ = dA * relu_dev(cache_net[f"output({l - 1})"])
+            gradients[f"dweight({l - 1})"] = (
+                    np.dot(data.T if l == 1 else cache_net[f"input({l - 2})"].T, dZ) / m
+            )
+            gradients[f"dbias({l - 1})"] = np.sum(dZ, axis=0, keepdims=True) / m
+
+        return gradients
+
+    def init_net(self, layers):
+        np.random.seed(123)
+        network = {}
+
+        for layer_ind, layer in enumerate(layers[1:]):
+            network[f"weight({layer_ind})"] = np.random.randn(layers[layer_ind], layer)
+            network[f"bias({layer_ind})"] = np.zeros((1, layer))
+
+        return network
+
+    def update_network(self, network, gradients, learning_rate):
+        layers_cnt = len(network) // 2 - 1
+
+        for layer_ind in range(layers_cnt):
+            network[f"weight({layer_ind})"] -= (
+                    learning_rate * gradients[f"dweight({layer_ind})"]
+            )
+            network[f"bias({layer_ind})"] -= (
+                    learning_rate * gradients[f"dbias({layer_ind})"]
+            )
+
+        return network
+
+    def train_model(self,
+                    data_train, result_train, layers, learning_rate=0.1, epochs=100, batch_size=64
+                    , freq=10):
+        network = self.init_net(layers)
+        notification_freq = max(epochs // freq, 10)
+        print_freq = epochs/10
+        global epochs_g,batch_g,layers_g
+        epochs_g = epochs
+        batch_g = batch_size
+        layers_g =layers
+        count = -1
+        for epoch in range(epochs):
+            for i in range(0, data_train.shape[0], batch_size):
+                data_batch = data_train[i: i + batch_size]
+                result_batch = result_train[i: i + batch_size]
+
+                res, cache_net = self.forward(data_batch, network)
+                gradients = self.backward(data_batch, result_batch, network, cache_net)
+
+                network = self.update_network(network, gradients, learning_rate)
+
+            if (epoch + 1) % notification_freq == 0 or epoch == 0:
+                result_current, cache_net = self.forward(data_train, network)
+                training_loss = mse(result_train, result_current)
+                self.probed_epoch.append(epoch + 1)
+                self.losses.append(training_loss)
+
+            if (epoch + 1) % print_freq == 0 or epoch == 0:
+                count += 1
+                result_current, cache_net = self.forward(data_train, network)
+                training_loss = mse(result_train, result_current)
+                self.prints[count] = {"key": (epoch+1),"value": training_loss}
 
 
-def init_net(layers):
-    np.random.seed(123)
-    network = {}
+        return network
 
-    for layer_ind, layer in enumerate(layers[1:]):
-        network[f"weight({layer_ind})"] = np.random.randn(layers[layer_ind], layer)
-        network[f"bias({layer_ind})"] = np.zeros((1, layer))
+    def evaluate_model(self, input_test, result_test, network):
+        global epochs_g, batch_g,layers_g
+        result_achived, _ = self.forward(input_test, network)
+        result_pred = np.argmax(result_achived, axis=1)
+        result_true = np.argmax(result_test, axis=1)
 
-    return network
+        f1 = f1_score(result_true, result_pred, average="micro")
+        print(f"E:{epochs_g}, B:{batch_g}, L:{layers_g}")
 
-
-def update_network(network, gradients, learning_rate):
-    layers_cnt = len(network) // 2 - 1
-
-    for layer_ind in range(layers_cnt):
-        network[f"weight({layer_ind})"] -= (
-            learning_rate * gradients[f"dweight({layer_ind})"]
-        )
-        network[f"bias({layer_ind})"] -= (
-            learning_rate * gradients[f"dbias({layer_ind})"]
-        )
-
-    return network
+        for values in self.prints.values():
+            print("Epoch:", values['key'], "Loss:",values['value'])
 
 
-def train_model(
-    data_train, result_train, layers, learning_rate=0.1, epochs=100, batch_size=64
-):
-    network = init_net(layers)
-    notification_freq = max(epochs // 10, 1)
-
-    for epoch in range(epochs):
-        for i in range(0, data_train.shape[0], batch_size):
-            data_batch = data_train[i : i + batch_size]
-            result_batch = result_train[i : i + batch_size]
-
-            res, cache_net = forward(data_batch, network)
-            gradients = backward(data_batch, result_batch, network, cache_net)
-
-            network = update_network(network, gradients, learning_rate)
-
-        if (epoch + 1) % notification_freq == 0 or epoch == 0:
-            result_current, cache_net = forward(data_train, network)
-            training_loss = mse(result_train, result_current)
-            print(f"Epoch: {epoch+1}, Loss: {training_loss}")
-
-    return network
-
-
-def evaluate_model(input_test, result_test, network):
-    result_achived, _ = forward(input_test, network)
-    result_pred = np.argmax(result_achived, axis=1)
-    result_true = np.argmax(result_test, axis=1)
-
-    accuracy = accuracy_score(result_true, result_pred)
-    f1 = f1_score(result_true, result_pred, average="micro")
-
-    # print(f"Accuracy: {accuracy}")
-    print(f"F1 Score: {f1}")
+        print(f"F1 Score: {f1}")
+        return f1
